b4320da1f0f58f7deaecef04b72f7645a355d301
Bern Lloyd S. Emperado <codenamebern@gmail.com>
2026-07-14 20:39:17 -0700

    First commit nako

---
+++ script.js
@@ -0,0 +1,20 @@
+const display = document.getElementById("display");
+
+ShowOnDisplay = (input) => {
+  display.value += input;
+};
+
+calculate = () => {
+  try {
+    display.value = eval(display.value);
+    display.style.color = "green";
+  } catch (mali) {
+    display.value = "Error";
+    display.style.color = "red";
+  }
+};
+
+clearDisplay = () => {
+  display.value = "";
+  display.style.color = "black";
+};
